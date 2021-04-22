f5dcdda95e920a6fba49d2a24e8515ecf592a22c
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-04-22 19:59:18 -0300

    Update pratica01.cpp

--- Pratica01/pratica01.cpp
+++ Pratica01/pratica01.cpp
@@ -12,24 +12,28 @@ using namespace std;
 	int main(){
 
 		cout << "Pratica01" << endl;
+		cout << "Primeira Aplicação C++" << endl;
 
 		Veiculo * obj1 = new Veiculo("v1");
 			{
 				Veiculo * obj2 = new Veiculo("v2");
 					{
 						Veiculo * obj3 = new Veiculo("v3");
-						obj1->setNumRodas(2);
-//						obj2->getNumRodas();
-						obj2->setNumRodas(3);
-						obj3 ->setNumRodas(8);
+							{
+								obj1->setNumRodas(2);
+//								obj2->getNumRodas();
+								obj2->setNumRodas(3);
+								obj3 ->setNumRodas(8);
 
-						delete obj3;
+								delete obj3;
+							}
+							delete obj2;
 					}
-					delete obj2;
+					delete obj1;
 			}
-			delete obj1;
-	}
-
-
+	Veiculo * veiculo4 = new Veiculo("v4");
+	veiculo4->setNumRodas(4);
+	delete veiculo4;
+}
 
 
