02e8218b6024a0f30aa0f6e457cd87833a9d5612
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-04-22 20:12:21 -0300

    Pratica 02

---
+++ Pratica02/pratica02.cpp
@@ -0,0 +1,18 @@
+/*
+ * pratica02.cpp
+ *
+ *  Created on: 22 de abr. de 2021
+ *      Author: melo
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Prática 02 C++!" << endl;
+    return 0;
+}
+
+
