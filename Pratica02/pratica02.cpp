dc78c4d8cdafe1bb20659415620a1abd44b3d471
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-04-25 20:06:26 -0300

    Uso de Herança Pública - classe Subclasse : public Superclasse{};

--- Pratica02/pratica02.cpp
+++ Pratica02/pratica02.cpp
@@ -2,59 +2,22 @@
  * pratica02.cpp
  *
  *  Created on: 22 de abr. de 2021
- *      Author: melo
+ *      Author: lmelo
  */
 
 #include <iostream>
-
 #include "veiculo.h"
 
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL,"");
-    cout << "Prática sobre herança" << endl<<endl;
-    /**Construtores iniciais*/
-    //Veiculo v1("normal");
-    //Aquatico aq1("aquatico");
-    //Terrestre t1("terrestre");
-    //Aereo a1("aereo");
-    Veiculo * terr = new Terrestre("VT1");
-    //((Terrestre *) terr) ->setCapacidadeMax(45);
-    dynamic_cast<Terrestre*> (terr) ->setCapacidadeMax(45);
-    /**Chamada inicial do método mover*/
-    //terr->mover();
-    //((Terrestre *) terr)->mover();
-    dynamic_cast<Terrestre*> (terr) ->mover();
-    Veiculo * aqua = new Aquatico("VQ1");
-    //((Aquatico *) aqua) -> setCargaMax(12.5);
-    dynamic_cast<Aquatico*> (aqua) -> setCargaMax(12.5);
-    /**Chamada inicial do método mover*/
-    //aqua->mover();
-    //((Aquatico *) aqua)->mover();
-    dynamic_cast<Aquatico*> (aqua) -> mover();
-    Veiculo * aereo = new Aereo("VA1");
-    //((Aereo *) aereo) ->setVelocidadeMax(1040.5);
-    dynamic_cast<Aereo*> (aereo) -> setVelocidadeMax(1040.5);
-    /**Chamada inicial do método mover*/
-    //aereo->mover();
-    //((Aereo *) aereo)->mover();
-    dynamic_cast<Aereo*> (aereo) -> mover();
-    Veiculo * anfi = new Anfibio("VAN1");
-    dynamic_cast<Anfibio*> (anfi) -> mover();
-    delete(aereo);
-    delete(aqua);
-    delete(terr);
-    delete(anfi);
-    /**Chamada inicial de criação e de métodos*/
-    //terr = new Terrestre("VT1");
-    //terr->setCapacidadeMax(45);
-    //aqua = new Aquatico("AQ1");
-    //aqua->setCargaMax(12.5);
-    //aereo = new Aereo("VA1");
-    //aereo->setVelocidadeMax(1040.5);
-    return 0;
+	Veiculo v1;
+	Terrestre t2;
+	Aereo ae3;
+	Aquatico aq4;
+
+return 0;
 
 }
 
--- Pratica02/veiculo.cpp
+++ Pratica02/veiculo.cpp
@@ -2,51 +2,12 @@
  * Veiculo.cpp
  *
  *  Created on: 22 de abr. de 2021
- *      Author: melo
+ *      Author: lmelo
  */
 
 #include <iostream>
 #include "veiculo.h"
 
 using namespace std;
-void Terrestre::setCapacidadeMax(int valor){
-    this->cap_pass=valor;
-}
 
-int Terrestre::getCapacidadeMax(){
-    return cap_pass;
-}
 
-void Terrestre::mover(){
-    cout<<"Movimentação terrestre: Veículo terreste "<<nome<<" moveu-se"<<endl<<endl;
-}
-
-void Aquatico::setCargaMax(float valor){
-    this->carga_max=valor;
-}
-
-float Aquatico::getCargaMax(){
-    return carga_max;
-}
-
-void Aquatico::mover(){
-    cout<<"Movimentação terrestre: Veículo aquatico "<<nome<<" moveu-se"<<endl<<endl;
-}
-
-void Aereo::setVelocidadeMax(float valor){
-    this->vel_max=valor;
-}
-
-float Aereo::getVelocidadeMax(){
-    return vel_max;
-}
-
-void Aereo::mover(){
-    cout<<"Movimentação terrestre: Veículo aereo "<<nome<<" moveu-se"<<endl<<endl;
-}
-
-void Anfibio::mover(){
-    //cout<<"Movimentação anfíbia: Veículo anfíbio "<<this->name<<" moveu-se"<<endl<<endl;
-    Terrestre::mover();
-    Aquatico::mover();
-}
