9363e2733375364b07f07ebabd29d91654f9fdd4
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-05-03 23:25:14 -0300

    2 - implementação de funções

--- Pratica03/funcoes.h
+++ Pratica03/funcoes.h
@@ -3,5 +3,25 @@
 #include <iostream>
 using namespace std;
 
+void trocar(int & a, int & b) {
+	int var;
+	var = a;
+	a=b;
+	b=var;
+}
+int maximo(const int a, const int b) {
+	if(a>b){
+		return a;
+	}else{
+		return b;
+	}
+}
+int minimo(const int a, const int b) {
+	if (a<b){
+		return a;
+	}else{
+		return b;
+	}
+}
 
 #endif // FUNCOES_H_INCLUDED
--- Pratica03/pratica03.cpp
+++ Pratica03/pratica03.cpp
@@ -8,7 +8,13 @@ using namespace std;
 
 int main()
 {
-	return 0;
+	int x = 10, y = 15, z = 30;
+	cout<<"Valor de X e Y respectivamente: "<<x<<" "<<y<<endl;
+	cout<<" Método swap"<<endl;
+	swap(x, y);
+	cout<<"Valor de X e Y respectivamente: "<<x<<" "<<y<<endl<<endl;
+	cout << "Mínimo entre " << x << " e " << y << " é: "<< minimo(x, y) << endl;
+	 cout << "Máximo entre " << x << " e " << y << " é: "<< maximo(x, y) << endl;
 }
 
 
