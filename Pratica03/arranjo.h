115aa92f3b624caafa90be955d7863a05921578c
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-05-03 22:45:37 -0300

    Prática 03 - Templates - Classes

--- Pratica03/arranjo.h
+++ Pratica03/arranjo.h
@@ -3,50 +3,7 @@
 #include <iostream>
 using namespace std;
 
-template <class T>
-class Arranjo
-{
-private:
-    int tamanho; //tamanho do arranjo
-    T * itens; //itens do arranjo
-public:
-    Arranjo(int tam)
-    {
-        //instanciar o array de itens com new (pratica 1) e seta tamanho
-        itens = new T[tam];
-        this->tamanho=tam;
-    }
 
-    virtual ~Arranjo()
-    {
-        //destruir o array de itens (prática 1)
-        delete [] itens;
-        cout<<"Array deletado"<<endl<<endl;
-    }
-
-    virtual T get(int idx)
-    {
-        //retornar um item do array a partir do índice
-        return itens[idx];
-    }
-
-    virtual void set(int idx, const T & item)
-    {
-        //set o item do array apontado pelo índice usando =
-        itens[idx] = item;
-    }
-    virtual void exibir();
-};
-
-template<class T>
-void Arranjo<T>::exibir()
-{
-    //exibir cada item numa linha da forma "<idx>: <item>"
-    for(int i=0; i<tamanho; i++)
-    {
-        cout<<"<"<<i<<">: <"<<itens[i]<<">"<<endl<<endl;
-    }
-}
 
 
 #endif // ARRANJO_H_INCLUDED
--- Pratica03/funcoes.h
+++ Pratica03/funcoes.h
@@ -3,53 +3,5 @@
 #include <iostream>
 using namespace std;
 
-template <class T>
-void trocar(T & a, T & b)
-{
-    T aux = a;
-    a=b;
-    b=aux;
-
-}
-
-template <class T>
-T maximo(const T a, const T b)
-{
-    if(a>b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-}
-
-template <class T>
-T minimo(const T a, const T b)
-{
-    if(a<b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-
-}
-
-template <>
-char * maximo<char *>(char * a, char * b)
-{
-    if(a>b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-}
 
 #endif // FUNCOES_H_INCLUDED
