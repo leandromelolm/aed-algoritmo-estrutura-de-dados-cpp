2303a7617bd5a2ebb81c89fbb4c7b1add4bdcc55
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-05-04 13:06:27 -0300

    Template: especializando a função maximo() para lidar com char *

--- Pratica03/funcoes.h
+++ Pratica03/funcoes.h
@@ -1,10 +1,14 @@
 #ifndef FUNCOES_H_INCLUDED
 #define FUNCOES_H_INCLUDED
 #include <iostream>
+#include<cstring>
+#include<string>
+
 using namespace std;
 
 template<class  T>
 void trocar(T & a, T & b) {
+	cout<<" Executando Método trocar()"<<endl;
 	T var;
 	var = a;
 	a=b;
@@ -26,5 +30,38 @@ T minimo(const T a, const T b) {
 		return b;
 	}
 }
+template<>
+char* maximo<char*>(char *a, char *b) {
+	 char * c="Teste de ordem alfabética";
+	int compara;
+	compara=strcmp(a,b);
+//	cout << "a diferença entre "<<a<<" e "<< b << " é  de "<< retorno << endl;
+//	if (compara < 0) {
+	if (strcmp(a,b)<0) {
+		cout << a << " na ordem alfabetica vem na frente de " <<b<<"."<<endl;
+//		return a;
+	} else if (compara == 0) {
+		cout << "strings são iguais" << endl;
+//		return 0;
+	} else if (compara > 0) {
+		cout << b << " na ordem alfabética vem na frente de "<<a<<"."<< endl;
+//		return b;
+	}
+	return c;
+}
+
+//	int compara = strcmp(a,b);
+////	cout <<a<<"  "<<b<<endl;
+//	switch (compara) {
+//	case -1:
+//		cout<<a<<"\n vem primeiro na ordem alfabetica"<<endl;
+//		break;
+//	case 0 :
+//		cout<<"\nstring são iguais"<<endl;
+//		break;
+//	case 1:
+//		cout<<b<<"\n vem primeiro na ordem alfabetica"<<endl;
+//		break;
+//	}
 
 #endif // FUNCOES_H_INCLUDED
--- Pratica03/pratica03.cpp
+++ Pratica03/pratica03.cpp
@@ -3,6 +3,7 @@
 #include "funcoes.h"
 #include "arranjo.h"
 #include "aluno.h"
+#include<string.h>
 
 using namespace std;
 
@@ -10,12 +11,26 @@ int main()
 {
 //	int x = 10, y = 15, z = 30;
 	float x = 5.5, y = 10.15, z = 30.7;
+	char a = 'a', b = 'b', c = 'c';
+	char str1[20]="gato";  char str2[20]="pato";  char str3[20]="cachorro";  char str4[20]="boi";  char str5[20]="pato";
+//	char str1[20]={'pato'}; char str2[20]={'gato'}; char str3[20]={'cachorro'};
+
 	cout<<"Valor de X e Y respectivamente: "<<x<<"   "<<y<<endl;
-	cout<<" Método Trocar()"<<endl;
 	trocar(x, y);
 	cout<<"Valor de X e Y respectivamente: "<<x<<"   "<<y<<endl<<endl;
+
 	cout << "Mínimo entre " << x << " e " << y << " é: "<< minimo(x, y) << endl;
-	 cout << "Máximo entre " << y << " e " << z << " é: "<< maximo(y, z) << endl;
+	cout << "Máximo entre " << y << " e " << z << " é: "<< maximo(y, z) << endl;
+
+	cout << "Mínimo entre " << a << " e " << b << " é: "<< minimo(a, b) << endl;
+	cout << "Máximo entre " << c << " e " << b << " é: "<< maximo(c, b) << endl;
+
+	cout<<"\nVerificação de ordem alfabética\n"<<endl;
+	cout << maximo(str1,str3) << endl;
+	cout <<maximo(str1,str2) << endl;
+	cout <<maximo(str4,str3) << endl;
+	cout <<maximo(str2,str5) << endl;
+
 }
 
 
