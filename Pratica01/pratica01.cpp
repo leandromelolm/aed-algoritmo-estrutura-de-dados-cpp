f837a905b3262ee3a0a8d8d4492fdca4ca0a8a76
horta <horta@Lg-S42>
2021-05-09 20:35:24 -0300

    alteração pratica 1

--- Pratica01/pratica01.cpp
+++ Pratica01/pratica01.cpp
@@ -35,7 +35,7 @@ using namespace std;
 				}
 				delete obj1;
 		}
-	Veiculo * veiculo4 = new Veiculo("carro");
+	Veiculo * veiculo4 = new Veiculo("carro1");
 	veiculo4->getNumRodas();
 	delete veiculo4;
 }
