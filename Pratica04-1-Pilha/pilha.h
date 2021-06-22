ad3a4e6de331d605778d326cbe06da982c373b04
LeandroMelo <Lg-S42>
2021-06-22 20:52:25 -0300

    comentários pilha.h - versões na linha 84 pilha2.h, linha 143 pilha3.h

--- Pratica04-1-Pilha/pilha.h
+++ Pratica04-1-Pilha/pilha.h
@@ -1,3 +1,87 @@
+#ifndef PILHA_H_
+#define PILHA_H_
+
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+
+class Pilha {
+private:
+	T *itens;
+	int cap_maxima;
+	int topo_pilha;
+public:
+	Pilha(int capacidade) {
+		this->cap_maxima= capacidade;
+		itens=new T[capacidade-1];
+		topo_pilha = 0;
+	}
+	~Pilha() {
+		delete []this->itens;
+	}
+	void empilha(T item) {
+		if(topo_pilha<cap_maxima){
+			itens[topo_pilha]=item;
+//			cout<<itens[topo_pilha]<<": "<<item<<endl;
+			this->topo_pilha = this->topo_pilha+1;
+		}else{
+			throw "Estouro de pilha";
+		}
+	}
+	T desempilha() {
+		if(topo_pilha>0){
+			topo_pilha = topo_pilha -1;
+		}else{
+			throw"Pilha vazia";
+		}
+		return itens[topo_pilha];
+	}
+	int tamanho() {
+		return this->topo_pilha;
+	}
+};
+#endif // PILHA_H_
+
+
+
+
+/*
+template <class T>
+class Pilha {
+private:
+	// Atributos para array de items, capacidade e topo da pilha
+public:
+	Pilha(int capacidade) {
+		// instancia array de items, inicializa capacidade e topo
+	}
+
+	~Pilha() {
+		// destroy array de items
+	}
+
+	void empilha(T item) {
+		// empilha um item no topo da pilha; lança “Estouro da pilha” se cheia
+	}
+
+	T desempilha() {
+		// remove um item do topo da pilha; lança “Pilha vazia” se vazia
+	}
+
+	int tamanho() {
+		// retorna o número de elementos na pilha.
+	}
+};
+
+*/
+
+
+
+
+/*
+
+//Pilha2.h - Testes OK
 
 #ifndef PILHA_H_
 #define PILHA_H_
@@ -49,34 +133,6 @@ public:
 };
 #endif // PILHA_H_
 
-
-/*
-template <class T>
-class Pilha {
-private:
-	// Atributos para array de items, capacidade e topo da pilha
-public:
-	Pilha(int capacidade) {
-		// instancia array de items, inicializa capacidade e topo
-	}
-
-	~Pilha() {
-		// destroy array de items
-	}
-
-	void empilha(T item) {
-		// empilha um item no topo da pilha; lança “Estouro da pilha” se cheia
-	}
-
-	T desempilha() {
-		// remove um item do topo da pilha; lança “Pilha vazia” se vazia
-	}
-
-	int tamanho() {
-		// retorna o número de elementos na pilha.
-	}
-};
-
 */
 
 
@@ -84,7 +140,7 @@ public:
 
 /*
 
-//Pilha.h - Testes OK
+//Pilha3.h - Testes OK
 
 #ifndef PILHA_H_INCLUDED
 #define PILHA_H_INCLUDED
