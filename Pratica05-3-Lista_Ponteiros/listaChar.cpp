4c86f8e5ca23d760995e237ec658730675030f26
LeandroMelo <Lg-S42>
2021-07-10 17:41:38 -0300

    retirando cout testes e comentários. código mais limpo. Lista Encadeada.

--- Pratica05-3-Lista_Ponteiros/listaChar.cpp
+++ Pratica05-3-Lista_Ponteiros/listaChar.cpp
@@ -9,34 +9,56 @@
 #include "lista_ligada.h"
 using namespace std;
 //mainChar
-int mainChar() {
-	Lista<char> lista(10);
+int main() {
+	try{
+		Lista<char> listaChar(10);
 
-	lista.adiciona('a');	// a
-	lista.adiciona('b');	// a b
-	lista.adiciona('c');	// a b c
-	lista.adiciona('d');	// a b c d
-	lista.adiciona('e');	// a b c d e
-	lista.exibe(); 			// a b c d e (saída)
+		listaChar.adiciona('a');	// a
+		listaChar.adiciona('b');	// a b
+		listaChar.adiciona('c');	// a b c
+		listaChar.adiciona('d');	// a b c d
+		listaChar.adiciona('e');	// a b c d e
+		listaChar.exibe(); 			// a b c d e (saída)
 
-	lista.remove(3); 		// a b d e
-	lista.exibe(); 			// a b d e (saída)
+		listaChar.remove(3); 		// a b d e
+		listaChar.exibe(); 			// a b d e (saída)
 
-	lista.pega(4); 			// e
-	lista.insere(2, 'f'); 	// a f b d e
-	lista.exibe(); 			// a f b d e (saída)
+		listaChar.pega(4); 			// e
+		listaChar.insere(2, 'f'); 	// a f b d e
+		listaChar.exibe(); 			// a f b d e (saída)
 
-	lista.adiciona('g'); 	// a f b d e g
-	lista.insere(4, 'h'); 	// a f b h d e g
-	lista.pega(2); 			// f
-	lista.exibe(); 			// a f b h d e g (saída)
+		listaChar.adiciona('g'); 	// a f b d e g
+		listaChar.insere(4, 'h'); 	// a f b h d e g
+		listaChar.pega(2); 			// f
+		listaChar.exibe(); 			// a f b h d e g (saída)
 
-	lista.insere(2, 'Z');
-	lista.exibe(); 			// a Z f b h d e g (saída)
+		listaChar.insere(2, 'Z');
+		listaChar.exibe(); 			// a Z f b h d e g (saída)
 
-	lista.insere(1, 'K');
-	lista.exibe(); 			// K a Z f b h d e g (saída)
+		listaChar.insere(1, 'K');
+		listaChar.exibe(); 			// K a Z f b h d e g (saída)
 
+		listaChar.insere(1, 'Y');
+		listaChar.exibe(); 			// Y K a Z f b h d e g (saída) 10 itens
+
+//		listaChar.insere(1, 'P');
+//		listaChar.exibe(); 			// P Y K a Z f b h d e g (saída) 11 itens
+//
+//		listaChar.insere(1, 'j');
+//		listaChar.exibe(); 			// j P Y K a Z f b h d e g (saída) 12 itens
+//
+//		listaChar.insere(7, 'c');
+//		listaChar.exibe(); 			// c j P Y K a Z f b h d e g (saída) 13 itens
+//
+//		listaChar.insere(17, 'E');  // Erro (Posição inválida
+//		listaChar.exibe(); 			// E c j P Y K a Z f b h d e g (saída) 14 itens
+
+		listaChar.tamanho();
+
+		cout<<"Tamanho da listaChar: "<<listaChar.tamanho();
+	}catch(const char *ex){
+		cerr << "Erro (" << ex << ")" << endl;
+	}
 	return 0;
 }
 
--- Pratica05-3-Lista_Ponteiros/lista_ligada.h
+++ Pratica05-3-Lista_Ponteiros/lista_ligada.h
@@ -40,6 +40,130 @@ public:
 			head = newNo;
 			tail = newNo;
 			tam_lista++;
+		} else {
+			tail->next = newNo;
+			tail = newNo;
+			tam_lista++;
+		}
+	}
+	T pega(int idx) {
+		if(idx < 1 || idx  > tam_lista) {
+		} else {
+			Node<T> *noAux = head;
+			for(int i = 1; i < idx; i++) {
+				noAux = noAux->next;
+			}
+			return noAux->item;
+		}
+	}
+	void insere (int idx, const T & item) {
+		Node<T> *newNo = new Node<T>;
+		Node<T> *tmp = head;
+
+		if (idx < 1 || idx > tam_maximo){ // Verifica se o índice é maior que o tamanho da lista
+			throw "Posição inválida";
+		}
+		if(tam_lista >= tam_maximo){
+			throw "Lista cheia";
+		}
+		newNo->item = item;
+		if(idx == 1) { //Primeiro elemento
+			newNo->next = head;
+			head = newNo;
+			tam_lista++;
+		}
+		else if(idx >= 1 && idx-1 <= tam_lista) {
+			for(int i = 1; i < idx-1; i++) {
+				tmp = tmp->next;
+			}
+			Node<T> *noAux = new Node<T>;
+			noAux->item = item;
+			noAux->next = tmp->next;
+			tmp->next = noAux;
+			tam_lista++;
+		}
+	}
+	void remove(int idx) {
+		if(idx < 1 || idx > tam_lista){
+			throw "Item inválido";
+		}else {
+			Node<T> *temp = head;
+			if(idx == 1) {
+				temp = head;
+				head = head->next;
+				if(tail == temp) {
+					tail = NULL;
+				}
+				tam_lista--;
+			} else if(idx > 1 && idx <= tam_lista) {
+				for(int i = 1; i < idx-1; i++) {
+					temp = temp->next;
+				}
+				Node<T> *noAux = temp->next;
+				temp->next = noAux->next;
+				if(tail == noAux) {
+					tail = temp;
+				}
+				delete noAux;
+				tam_lista--;
+			}
+		}
+	}
+	void exibe() {
+		Node<T> *noAux = head;
+		while(noAux != NULL ){
+			cout << noAux->item << " ";
+			noAux = noAux->next;
+		}
+		cout << endl;
+	}
+	int tamanho() {
+		return tam_lista;
+	}
+};
+
+#endif // LISTA_LIGADA_H_INCLUDED
+
+
+/*
+
+template <class T>
+struct Node {
+	Node *next; //Ponteiro para próximo elemento
+	T item;
+};
+
+template <class T>
+class Lista {
+private:
+	Node<T> *head;	//Ponteiro para início da lista
+	Node<T> *tail; //Ponteiro para último elemento
+	int tam_lista;
+	int tam_maximo;
+public:
+	Lista(int capacidade) {
+
+		tam_lista = 0;
+		tam_maximo = capacidade;
+		head = NULL;
+		tail = NULL;
+		cout<<"Lista Encadeada\n";
+	}
+	~Lista() {
+		for(int i = 0; i < tam_lista; i++) {
+			remove(1);
+		}
+	}
+	void adiciona (const T & item) {
+		Node<T> *newNo = new Node<T>;
+		newNo->item = item;
+		if(tam_lista >= tam_maximo){
+			throw "Lista cheia";
+		}
+		if(head == NULL) {
+			head = newNo;
+			tail = newNo;
+			tam_lista++;
 			cout<<"add->"<<newNo->item<<"\n";
 		} else {
 			tail->next = newNo;
@@ -129,60 +253,6 @@ public:
 		return tam_lista;
 	}
 };
-
-#endif // LISTA_LIGADA_H_INCLUDED
-
-
-/*
-void insere (int idx, const T & item) {
-		nodeLista<T> *newNo = new nodeLista<T>;
-
-		nodeLista<T> *noAnterior = inicialNo;
-
-//		if (idx < 1 && idx > tam_maximo){
-//			throw "Item inválido";
-//		}
-		if(tam_lista >= tam_maximo){
-			throw "Lista cheia";
-		}
-//		noAnterior->item = item; // ERRO - altera no inicio
-		newNo->item = item;
-
-
-		if(idx == 1) { //Primeiro elemento
-//			noAnterior->next = inicialNo;
-//			noAnterior->next = inicialNo;
-//			newNo->next = inicialNo;
-			newNo->next = inicialNo;
-
-
-			cout<<newNo<<" "<<inicialNo<<"\n";
-//			inicialNo->next = inicialNo;
-//			inicialNo->next = newNo->item;
-//			newNo = inicialNo->next;
-			inicialNo = newNo;
-
-
-			cout<<"insereInicio->"<<newNo->item<<"\n";
-
-
-			cout<<"insereInicio->"<<item<<"\n";
-
-//			cout<<"insereInicio->"<<(*newNo).item<<"\n";  // "(*newNo).item" é igual a "newNo->item"
-
-
-		} else if(idx >= 1 && idx-1 <= tam_lista) {
-			for(int i = 1; i < idx-1; i++) {
-				noAnterior = noAnterior->next;
-			}
-			nodeLista<T> *noAux = new nodeLista<T>;
-			noAux->item = item;
-			noAux->next = noAnterior->next;
-			noAnterior->next = noAux;
-			tam_lista++;
-//			cout<<"insere->"<<item<<"\n";
-		}
-	}
 
 */
 
