f9c1880d22ef74697e34fa4e1b620458c5070b33
LeandroMelo <Lg-S42>
2021-07-20 13:01:40 -0300

    Corrindo ordenação -  busca sequencial apos remoção - corrigido

--- Pratica06-1-ListaOrdenada_e_BuscaBinaria/ordenada.cpp
+++ Pratica06-1-ListaOrdenada_e_BuscaBinaria/ordenada.cpp
@@ -25,16 +25,16 @@ public:
 		}
 		items[i] = key;
 		tamanho++;
-		exibe();
     }
 
 	int buscaSequencial(int key) {
-		for(int i=0; i < tamanho; i++) {
-			if (key < items[i])
-				return -1;
-			if(key == items[i])
-				return i;
-	    }
+        for(int i=0; i < tamanho; i++) {
+        	if (key < items[i])
+        		return -1;
+            if(key == items[i])
+                return i;
+        }
+        return -1;
 	}
 
 	int buscaBinaria(int item) {
@@ -56,14 +56,13 @@ public:
 	}
 
     void remove(int item){
-        int id = buscaBinaria(item);
-        if(id != -1){
-            for(int i = id; i < tamanho; i++){
+        int idx = buscaBinaria(item);
+        if(idx != -1){
+            for(int i = idx; i < tamanho; i++){
                 items[i] = items[i + 1];
             }
             tamanho--;
         }
-        exibe();
 	}
 
 private:
@@ -85,7 +84,6 @@ private:
 
 };
 
-//int main_Ordenada () {
 int main() {
 
 	ListaOrdenada lista(10);
@@ -104,7 +102,8 @@ int main() {
 	int teste [] = {5, 7, 16, 99, 45, 12, 33, 1, 60, 6};
 
 	for (int i = 0; i < 10; i++) {
-		cout << "Buscando " << teste[i] << ": sequencial = " << lista.buscaSequencial(teste[i]) << " binaria = " << lista.buscaBinaria(teste[i]) << endl;
+		cout << "Buscando " << teste[i] << ": sequencial = " << lista.buscaSequencial(teste[i])
+				<< " binaria = " << lista.buscaBinaria(teste[i]) << endl;
 
 	}
 
@@ -115,8 +114,13 @@ int main() {
 	lista.remove(12);
 	lista.remove(1);
 
+	cout<<"Lista pós remoção - ";
+	cout << "Lista válida: " << (lista.valida()?"sim":"não") << endl;
+	lista.exibe();
+
 	for (int i = 0; i < 10; i++) {
-		cout << "Buscando " << teste[i] << ": sequencial = " << lista.buscaSequencial(teste[i]) << " binaria = " << lista.buscaBinaria(teste[i]) << endl;
+		cout << "Buscando " << teste[i] << ": sequencial = " << lista.buscaSequencial(teste[i])
+				<< " binaria = " << lista.buscaBinaria(teste[i]) << endl;
 
 	}
 
@@ -134,15 +138,63 @@ int main() {
 
 /*
 
+// Com codigo para exibir saídas no console
+
+	void insere(int key) {
+    	int i =tamanho;
+		while(( i>0) &&  (key < items[i-1])) {
+			items[i] = items[i-1];
+			i--;
+		}
+		items[i] = key;
+		tamanho++;
+//		cout<<"insere "<<items[i]<<"    ";
+//		exibe();
+    }
+
 	int buscaSequencial(int key) {
-		for(int i=0; i < tamanho; i++) {
-			cout<<" ("<<key<<" = "<<items[i]<<") ";
-			if (key < items[i])
-				return -1;
-			if(key == items[i])
-				return i;
-	    }
+        for(int i=0; i < tamanho; i++) {
+//        	cout<<" ("<<key<<" = "<<items[i]<<") ";
+        	if (key < items[i])
+        		return -1;
+            if(key == items[i])
+                return i;
+        }
+        return -1;
 	}
+
+	int buscaBinaria(int item) {
+		return buscaBinaria(0, tamanho - 1, item);
+	}
+
+	int valida() {
+		for (int i = 0; i < tamanho - 1; i++) {
+			if (items[i] > items[i + 1]) return 0;
+		}
+		return 1;
+	}
+
+	void exibe() {
+		for (int i = 0; i < tamanho; i++) {
+			cout << i << ": " << items[i] << "; ";
+		}
+		cout << endl;
+	}
+
+    void remove(int item){
+        int idx = buscaBinaria(item);
+//        cout<<"índice "<<idx<<"  ";
+//        cout<<"remove "<<item<<"  ";
+
+        if(idx != -1){
+            for(int i = idx; i < tamanho; i++){
+                items[i] = items[i + 1];
+            }
+            tamanho--;
+        }
+//        exibe();
+	}
+
 
 */
 
